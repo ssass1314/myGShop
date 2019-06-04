51b592d4b9c0d61ed60675002ed8dc83330bc445
xys <aseng_xys@com>
2019-06-04 15:47:34 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+ <div id="div">
+     <router-view/>
+    <FooterGuide></FooterGuide>
+ </div>
+</template>
+
+<script type="text/ecmascript-6">
+import FooterGide from './components/FooterGuide/FooterGuide'
+export default {
+  components: {
+    FooterGide,
+  }
+}
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus" scoped>
+  
+</style>
